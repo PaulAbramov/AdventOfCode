fda78f5a57343dffbb5afc5af8d2c250383fb20a
Paul Abramov <paul.abramov1911@gmail.com>
2022-12-14 16:12:53 +0100

    finish Day 1.1 and create Day 1.2

--- Day1.1/Program.cs
+++ Day1.1/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Tuple<int, long>? biggestAmountOfCalories = null;
+int counter = 0;
+long currentElvCalories = 0;
+foreach (var line in File.ReadLines("input.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        biggestAmountOfCalories ??= new Tuple<int, long>(counter, currentElvCalories);
+
+        if (biggestAmountOfCalories.Item2 < currentElvCalories)
+        {
+            biggestAmountOfCalories = new Tuple<int, long>(counter, currentElvCalories);
+        }
+        
+        counter++;
+        currentElvCalories = 0;
+        
+        continue;
+    }
+    
+    currentElvCalories += long.Parse(line);
+}
+
+
+if (biggestAmountOfCalories != null)
+{
+    Console.WriteLine(
+        $"The {biggestAmountOfCalories.Item1}. Elv has the most Calories with {biggestAmountOfCalories.Item2}");
+}
