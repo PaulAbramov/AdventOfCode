1f6df370de1b9091b3500ee0a99f7982ec1b2c7b
Paul Abramov <paul.abramov1911@gmail.com>
2022-12-25 21:45:46 +0100

    Finished Day 5.2

---
+++ Day5_2/CrateStackerCrane.cs
@@ -0,0 +1,82 @@
+﻿namespace Day5_2;
+
+public class CrateStackerCrane
+{
+    private readonly List<Stack<char>> crates = new List<Stack<char>>();
+
+    public void Initialize(List<string> stackLines)
+    {
+        var amountOfStacks = stackLines.First().Count(x => x.Equals('['));
+        for (var i = 0; i < amountOfStacks; i++)
+        {
+            crates.Add(new Stack<char>());
+        }
+
+        foreach (var stackline in stackLines)
+        {
+            PlaceCrateFromLine(string.Concat(" ", stackline));
+        }
+    }
+
+    /// <summary>
+    /// Go through the line from a startindex and search for more crates
+    /// we always have a empty space (1 char) before a crate (3 chars) = 4 chars
+    /// </summary>
+    /// <param name="stackLine"></param>
+    /// <param name="startIndex"></param>
+    private void PlaceCrateFromLine(string stackLine, int startIndex = 0)
+    {
+        var index = stackLine.IndexOf('[', startIndex);
+        if (index == -1)
+        {
+            return;
+        }
+
+        var crate = stackLine[index + 1];
+        var stackIndex = (int)(index / 4);
+        crates[stackIndex].Push(crate);
+
+        PlaceCrateFromLine(stackLine, index + 2);
+    }
+
+    /// <summary>
+    /// Pop from one, and push the returned element to the other stack
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="fromStack"></param>
+    /// <param name="toStack"></param>
+    public void MoveCrates(int amount, int fromStack, int toStack)
+    {
+        var tempStack = new Stack<char>();
+
+        for (var i = 0; i < amount; i++)
+        {
+            if (crates[fromStack - 1].Count > 0)
+            {
+                tempStack.Push(crates[fromStack - 1].Pop());
+            }
+        }
+
+        while (tempStack.Count > 0)
+        {
+            crates[toStack - 1].Push(tempStack.Pop());
+        }
+    }
+
+    /// <summary>
+    /// call the peek method on every Stack and add the crate element to the string
+    /// </summary>
+    /// <returns></returns>
+    public string ReturnTopCratesPerStack()
+    {
+        return crates.Aggregate(string.Empty, (current, cratesStack) =>
+        {
+            if (cratesStack.Count > 0)
+            {
+                return current + cratesStack.Peek();
+            }
+
+            return current;
+        });
+    }
+}
---
+++ Day5_2/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * As you watch the crane operator expertly rearrange the crates,
+ * you notice the process isn't following your prediction.
+ * 
+ * Some mud was covering the writing on the side of the crane,
+ * and you quickly wipe it away.
+ * The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+ *
+ * The CrateMover 9001 is notable for many new and
+ * exciting features: air conditioning, leather seats, an extra cup holder,
+ * and the ability to pick up and move multiple crates at once.
+ *
+ * Before the rearrangement process finishes,
+ * update your simulation so that the Elves know where they should stand to be ready to unload the final supplies.
+ * After the rearrangement procedure completes, what crate ends up on top of each stack?
+ */
+
+using Day5_2;
+
+var lines = File.ReadLines("input.txt").ToList();
+var stacklines = new List<string>();
+
+CrateStackerCrane crane = new CrateStackerCrane();
+
+foreach (var line in lines)
+{
+    if (line.Any(x => x.Equals('[')))
+    {
+        stacklines.Add(line);
+        continue;
+    }
+
+    if (string.IsNullOrEmpty(line))
+    {
+        stacklines.Reverse();
+
+        crane.Initialize(stacklines);
+
+        continue;
+    }
+
+    if (line.StartsWith("move"))
+    {
+        var indexOfFrom = line.IndexOf("from", StringComparison.Ordinal);
+        var indexOfTo = line.IndexOf("to", StringComparison.Ordinal);
+
+        var amountString = line.Substring(5, indexOfFrom - 5).Trim();
+        var amount = int.Parse(amountString);
+
+        var fromStackString = line.Substring(indexOfFrom + 5, 1).Trim();
+        var fromStack = int.Parse(fromStackString);
+
+        var toStackString = line.Substring(indexOfTo + 2);
+        var toStack = int.Parse(toStackString);
+
+        crane.MoveCrates(amount, fromStack, toStack);
+    }
+}
+
+Console.WriteLine(crane.ReturnTopCratesPerStack());
