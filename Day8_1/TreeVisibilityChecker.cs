760dd283489950df8f0f0438e41584cc90519d70
Paul Abramov <paul.abramov1911@gmail.com>
2022-12-27 00:02:30 +0100

    Finished Day 8.1 Part 2

---
+++ Day8_1/TreeVisibilityChecker.cs
@@ -0,0 +1,118 @@
+﻿namespace Day8_1;
+
+public static class TreeVisibilityChecker
+{
+    /// <summary>
+    /// Row and Column have the same sizes(?)
+    /// Check which quadrant the number is in, so possibly shorten the time of checks
+    /// </summary>
+    /// <param name="xPosition"></param>
+    /// <param name="yPosition"></param>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public static bool IsVisibleFromOutside(int xPosition, int yPosition, int[,] grid)
+    {
+        List<Func<bool>> functions = new List<Func<bool>>();
+        
+        var rowLength = (int) Math.Sqrt(grid.Length);
+        
+        if (xPosition == 0 
+            || yPosition == 0
+            || yPosition == rowLength - 1 
+            || xPosition == rowLength - 1)
+        {
+            return true;
+        }
+
+        int treeHeightToCompare = grid[xPosition, yPosition];
+        
+        // Calculate in which quadrant we are
+        int quadrant = (xPosition / (rowLength / 2)) * 2 + (yPosition / (rowLength / 2));
+        switch (quadrant)
+        {
+            // top left quadrant
+            case 0:
+                functions.Add(bool () => VisibleCheckFromLeft(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromTop(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromRight(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromDown(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                break;
+            // top right quadrant
+            case 1:
+                functions.Add(bool () => VisibleCheckFromRight(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromTop(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromLeft(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromDown(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                break;
+            // bottom left quadrant
+            case 2:
+                functions.Add(bool () => VisibleCheckFromLeft(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromDown(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromRight(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromTop(xPosition, yPosition, grid, treeHeightToCompare));
+                break;
+            // bottom right quadrant
+            case 3: 
+                functions.Add(bool () => VisibleCheckFromRight(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromDown(xPosition, yPosition, grid, rowLength, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromLeft(xPosition, yPosition, grid, treeHeightToCompare));
+                functions.Add(bool () => VisibleCheckFromTop(xPosition, yPosition, grid, treeHeightToCompare));
+                break;
+        }
+
+        // Execute all checks in the order
+        return functions.Any(function => function());
+    }
+
+    private static bool VisibleCheckFromLeft(int xPosition, int yPosition, int[,] grid, int valueToCompare)
+    {
+        for (int i = yPosition - 1; i >= 0; i--)
+        {
+            if (grid[xPosition, i] >= valueToCompare)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static bool VisibleCheckFromRight(int xPosition, int yPosition, int[,] grid, int rowLength, int valueToCompare)
+    {
+        for (int i = yPosition + 1; i < rowLength; i++)
+        {
+            if (grid[xPosition, i] >= valueToCompare)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static bool VisibleCheckFromTop(int xPosition, int yPosition, int[,] grid, int valueToCompare)
+    {
+        for (int i = xPosition - 1; i >= 0; i--)
+        {
+            if (grid[i, yPosition] >= valueToCompare)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static bool VisibleCheckFromDown(int xPosition, int yPosition, int[,] grid, int rowLength, int valueToCompare)
+    {
+        for (int i = xPosition + 1; i < rowLength; i++)
+        {
+            if (grid[i, yPosition] >= valueToCompare)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
